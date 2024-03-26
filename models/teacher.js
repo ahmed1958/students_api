0acea43482cc941b21d2aa241968f23be2af81d2
ahmedaliv <ahmed195131@feng.bu.edu.eg>
2024-03-27 00:32:54 +0200

    Teacher Router & Model

---
+++ models/teacher.js
@@ -0,0 +1,6 @@
+export const Teacher = [
+  { id: 1, name: "Seko", email: "seko@gmail.com" },
+  { id: 2, name: "Nagy", email: "nagy@gmail.com" },
+  { id: 3, name: "Ahmed Ali", email: "ahmed@gmail.com" },
+  { id: 4, name: "Mohamed Mamdouh", email: "mmdh@gmail.com" },
+];
---
+++ routes/teacher_router.js
@@ -0,0 +1,11 @@
+import { Router } from "express";
+import * as teacher_controller from "../controllers/Teacher_controller.js";
+
+const teacherRouter = Router();
+teacherRouter.get("/", teacher_controller.getAllTeacher);
+teacherRouter.get("/:id", teacher_controller.getTeacherById);
+teacherRouter.post("/", teacher_controller.addTeacher);
+teacherRouter.put("/:id", teacher_controller.editTeacherById);
+teacherRouter.delete("/:id", teacher_controller.deleteTeacherById);
+
+export { teacherRouter };
