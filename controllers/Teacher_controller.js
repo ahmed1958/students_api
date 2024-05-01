79e7c4747c1664fdb8b4dd01c7e22145efa88248
nagy nabil <nagynabil651@gmail.com>
2024-05-02 00:46:18 +0300

    chore: update teacher_controller

--- controllers/Teacher_controller.js
+++ controllers/Teacher_controller.js
@@ -12,12 +12,16 @@ function validate_Teacher(teacher) {
   return schema.validate(teacher);
 }
 
-const getAllTeacher = (req, res) => {
-  res.send(Teacher);
+const getAllTeacher = async (req, res) => {
+  const index = await Teacher.find();
+  res.json({
+    data: index,
+  });
 };
 
-const getTeacherById = (req, res) => {
-  const teacher = Teacher.find((c) => c.id === parseInt(req.params.id));
+const getTeacherById = async (req, res) => {
+  const id = req.params.id;
+  const teacher = await Teacher.findById(id);
   if (!teacher) {
     res.status(404).send("the teacher with the given id not found");
   } else {
@@ -25,39 +29,40 @@ const getTeacherById = (req, res) => {
   }
 };
 
-const addTeacher = (req, res) => {
+const addTeacher = async (req, res) => {
   const result = validate_Teacher(req.body);
   if (result.error) {
     res.sendStatus(400).json({ msg: result.error.details[0].message });
     return;
   }
-  const teacher = {
-    id: Teacher[Teacher.length - 1].id + 1,
+
+  const teacher = await Teacher.create({
     name: req.body.name,
     email: req.body.email,
-  };
-  Teacher.push(teacher);
-  res.send(Teacher);
+  });
+  return res.json(teacher);
 };
 
-const editTeacherById = (req, res) => {
-  const teacher = Teacher.find((c) => c.id === parseInt(req.params.id));
+const editTeacherById = async (req, res) => {
+  const id = req.params.id;
+
+  const { name, email } = req.body;
+  const teacher = await Teacher.findOneAndUpdate(
+    { _id: id },
+    { name, email },
+    {
+      new: true,
+    },
+  );
   if (!teacher) res.status(404).send("the teacher with the given id not found");
 
-  if (req.body.name) {
-    teacher.name = req.body.name;
-  }
-  if (req.body.email) {
-    teacher.email = req.body.email;
-  }
-  res.send(teacher);
+  return res.send(teacher);
 };
 
-const deleteTeacherById = (req, res) => {
-  const teacher = Teacher.find((c) => c.id === parseInt(req.params.id));
+const deleteTeacherById = async (req, res) => {
+  const id = req.params.id;
+  const teacher = await Teacher.findOneAndDelete({ _id: id });
   if (!teacher) res.status(404).send("the teacher with the given id not found");
-  const index = Teacher.indexOf(teacher);
-  Teacher.splice(index, 1);
   res.send(teacher);
 };
 
