5a6fbadc55b152021afb210ee02184a10b1160ad
nagy nabil <nagynabil651@gmail.com>
2024-05-02 00:44:44 +0300

    chore: students model

--- models/courses.js
+++ models/courses.js
@@ -1,19 +1,29 @@
 //u must create a file for each database(objects for now) and then in model.export put the object name
 import mongoose from "mongoose";
+import {
+  CourseSchemaName,
+  StudentSchemaName,
+  TeacherSchemaName,
+} from "./helpers.js";
 const Schema = mongoose.Schema;
 const model = mongoose.model;
 
 const courseSchema = Schema({
-    name: {
-        type: String,
-        required: true,
+  name: {
+    type: String,
+    required: true,
+  },
+  instructor_id: {
+    type: Schema.Types.ObjectId,
+    ref: TeacherSchemaName,
+    required: true,
+  },
+  students: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: StudentSchemaName,
     },
-    instructor_id: {
-        type: Schema.Types.ObjectId, // Specify the type as ObjectId
-        ref: 'Instructor' // Reference to the 'Instructor' model
-    }
+  ],
 });
 
-export const Course = model('Course', courseSchema);
-
-
+export const Course = model(CourseSchemaName, courseSchema);
---
+++ models/helpers.js
@@ -0,0 +1,7 @@
+const CourseSchemaName = "Course";
+
+const StudentSchemaName = "Student";
+
+const TeacherSchemaName = "Teacher";
+
+export { CourseSchemaName, StudentSchemaName, TeacherSchemaName };
