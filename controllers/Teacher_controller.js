7316a5d3e6a786518972de40be8e55628f069a31
nagy nabil <nagynabil651@gmail.com>
2024-05-02 02:16:46 +0300

    fix: teacher controller problems

--- controllers/Teacher_controller.js
+++ controllers/Teacher_controller.js
@@ -15,14 +15,14 @@ function validate_Teacher(teacher) {
 
 const getAllTeacher = async (req, res) => {
   const index = await Teacher.find();
-  res.json({
+  return res.json({
     data: index,
   });
 };
 
 const getTeacherById = async (req, res) => {
   const id = req.params.id;
-  const teacher = await Teacher.findById(id);
+  const teacher = await Teacher.findById(id).populate("courses");
   if (!teacher) {
     res.status(404).send("the teacher with the given id not found");
   } else {
@@ -33,20 +33,42 @@ const getTeacherById = async (req, res) => {
 const addTeacher = async (req, res) => {
   const result = validate_Teacher(req.body);
   if (result.error) {
-    res.sendStatus(400).json({ msg: result.error.details[0].message });
+    res.status(400).json({ msg: result.error.details });
     return;
   }
 
-  const teacher = await Teacher.create({
-    name: req.body.name,
-    email: req.body.email,
-  });
-  return res.json(teacher);
+  try {
+    const teacher = await Teacher.create({
+      name: req.body.name,
+      email: req.body.email,
+    });
+    return res.json(teacher);
+  } catch (e) {
+    if (e.code === 11000) {
+      return res.status(400).send("Email already exists");
+    }
+    res.status(500).send("server error");
+    throw e;
+  }
 };
 
+function validatePutTeacher(teacher) {
+  const schema = joi.object({
+    name: joi.string().min(3).optional(),
+    email: joi
+      .string()
+      .email({ minDomainSegments: 2, tlds: { allow: ["com"] } })
+      .optional(),
+  });
+  return schema.validate(teacher);
+}
 const editTeacherById = async (req, res) => {
   const id = req.params.id;
-
+  const result = validatePutTeacher(req.body);
+  if (result.error) {
+    res.status(400).json({ msg: result.error.details });
+    return;
+  }
   const { name, email } = req.body;
   const teacher = await Teacher.findOneAndUpdate(
     { _id: id },
@@ -73,7 +95,7 @@ const deleteTeacherById = async (req, res) => {
 
 function validateEnrollmentReq(course) {
   const schema = joi.object({
-    courseId: joi.string(),
+    courseId: joi.string().required(),
   });
   return schema.validate(course);
 }
@@ -82,7 +104,7 @@ async function enrollTeacherIntoCourse(req, res) {
   const { id } = req.params;
   const validation = validateEnrollmentReq(req.body);
   if (validation.error) {
-    res.sendStatus(400).json({ msg: validation.error.details });
+    return res.status(400).json({ msg: validation.error.details });
   }
   const { courseId } = req.body;
   const teacher = await Teacher.findById(id);
@@ -96,8 +118,7 @@ async function enrollTeacherIntoCourse(req, res) {
 
   teacher.courses.addToSet(course);
   course.instructor_id = teacher;
-  await teacher.save();
-  await course.save();
+  await Promise.all([teacher.save(), course.save()]);
   return res.send(teacher);
 }
 
--- models/teacher.js
+++ models/teacher.js
@@ -9,6 +9,7 @@ const teacherSchema = new mongoose.Schema({
   email: {
     type: String,
     required: true,
+    unique: true,
   },
   courses: [
     {
