074641151638bc974a749122d414080021a2636f
Ahmed Awwad <ahmedawwad332@gmail.com>
2024-05-02 14:10:25 +0300

    Modify Student router

--- routes/student_router.js
+++ routes/student_router.js
@@ -1,11 +1,14 @@
+ts
 import { Router } from "express";
 import * as student_controller from "../controllers/Student_controller.js";
 
 const studentRouter = Router();
 studentRouter.get("/", student_controller.getAllStudent);
 studentRouter.get("/:id", student_controller.geStudentById);
-studentRouter.post("/", student_controller.addStudent);
+studentRouter.post("/", student_controller.create);
 studentRouter.put("/:id", student_controller.editStudentById);
 studentRouter.delete("/:id", student_controller.deleteStudent);
+studentRouter.put("/:id/enroll", student_controller.enrollInCourse);
+studentRouter.put("/:id/decline", student_controller.declineCourse);
 
-export { studentRouter };
+export { studentRouter };
