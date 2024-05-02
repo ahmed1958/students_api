9bc9d40ee3def6237118bb44399dbc40f8fb170a
Ahmed Awwad <ahmedawwad332@gmail.com>
2024-05-02 14:07:42 +0300

    Modify Student controller

--- controllers/Student_controller.js
+++ controllers/Student_controller.js
@@ -1,7 +1,9 @@
+ts
 import joi from "joi";
 import { Student } from "../models/student.js";
+import { Course } from "../models/courses.js";
 
-function validate_Student(student) {
+function validateStudent(student) {
   const schema = joi.object({
     name: joi.string().min(3).required(),
     email: joi
@@ -13,71 +15,171 @@ function validate_Student(student) {
 }
 
 ////////////////////////////////////
-const getAllStudent = (req, res) => {
-  
-  res.send(Student);
+const getAllStudent = async (req, res) => {
+  const students = await Student.find();
+  res.json({
+    data: students,
+  });
 };
 
 ////////////////////////////////////////////
 
-const geStudentById = (req, res) => {
-  const student = Student.find((c) => c.id === parseInt(req.params.id));
+const geStudentById = async (req, res) => {
+  const id = req.params.id;
+  const student = await Student.findById(id).populate("courses");
   if (!student) {
-    res.status(404).send("the student with the given id not found");
+    return res.status(404).send("the student with the given id not found");
   } else {
-    res.send(student);
+    return res.json(student);
   }
 };
 
 /////////////////////////////////////////////////////
-const addStudent = (req, res) => {
-  const result = validate_Student(req.body);
+const create = async (req, res) => {
+  const result = validateStudent(req.body);
   if (result.error) {
-    res.sendStatus(400).json({ msg: result.error.details[0].message });
-    return;
+    return res.status(400).json({ msg: result.error.details });
   }
-  const student = {
-    id: Student[Student.length - 1].id + 1,
-    name: req.body.name,
-    email: req.body.email,
-  };
-  Student.push(student);
-  res.send(Student);
+  try {
+    const student = await Student.create({
+      name: req.body.name,
+      email: req.body.email,
+    });
+    return res.json(student);
+  } catch (e) {
+    if (e.code === 11000) {
+      return res.status(400).send("Email already exists");
+    }
+  }
 };
 
 //////////////////////////////////////////////////////////////////
 
-const editStudentById = (req, res) => {
-  const student = Student.find((c) => c.id === parseInt(req.params.id));
-  if (!student) res.status(404).send("the student with the given id not found");
-  const result = validate_Student(req.body);
+function validatePutStudent(student) {
+  const schema = joi.object({
+    name: joi.string().min(3).optional(),
+    email: joi
+      .string()
+      .email({ minDomainSegments: 2, tlds: { allow: ["com", "net"] } })
+      .optional(),
+  });
+  return schema.validate(student);
+}
+const editStudentById = async (req, res) => {
+  const id = req.params.id;
+  const result = validatePutStudent(req.body);
   if (result.error) {
-    res.sendStatus(400).send(result.error.details[0].message);
-    return;
+    return res.status(400).send(result.error.details[0].message);
   }
-  if (req.body.name) {
-    student.name = req.body.name;
+  const { name, email } = req.body;
+  try {
+    const student = await Student.findOneAndUpdate(
+      { _id: id },
+      { name, email },
+      { new: true },
+    );
+    if (!student)
+      res.status(404).send("the student with the given id not found");
+    return res.send(student);
+  } catch (e) {
+    console.log(
+      "🪵 [Student_controller.js:82] ~ token ~ \x1b[0;32me\x1b[0m = ",
+      e,
+    );
+    return res.status(400).send(e.message);
   }
-  if (req.body.email) {
-    student.email = req.body.email;
-  }
-  res.send(Student);
 };
 /////////////////////////////////////////////////////////////
 
 //////////////////////////////////////////////////////
-const deleteStudent = (req, res) => {
-  const student = Student.find((c) => c.id === parseInt(req.params.id));
-  if (!student) res.status(404).send("the student with the given id not found");
-  const index = Student.indexOf(student);
-  Student.splice(index, 1);
-  res.send(Student);
+const deleteStudent = async (req, res) => {
+  const id = req.params.id;
+  const student = await Student.findOneAndDelete({ _id: id });
+  if (!student)
+    return res.status(404).send("the student with the given id not found");
+  // remove student from courses
+  await Course.updateMany(
+    { students: { $in: [req.params.id] } },
+    { $pull: { students: id } },
+  );
+  return res.send(student);
 };
 
+/**
+ * validate
+ * {
+ *  courseId: string
+ * }
+ */
+function validateEnrollmentReq(course) {
+  const schema = joi.object({
+    courseId: joi.string().required(),
+  });
+  return schema.validate(course);
+}
+async function enrollInCourse(req, res) {
+  const id = req.params.id;
+  const result = validateEnrollmentReq(req.body);
+  if (result.error) {
+    return res.status(400).json({ msg: result.error.details });
+  }
+  try {
+    const { courseId } = req.body;
+    const [student, course] = await Promise.all([
+      Student.findById(id),
+      Course.findById(courseId),
+    ]);
+    if (!student) {
+      return res.status(404).send("the student with the given id not found");
+    }
+    if (!course) {
+      return res.status(404).send("the course with the given id not found");
+    }
+    student.courses.addToSet(course);
+    course.students.addToSet(student);
+    await Promise.all([student.save(), course.save()]);
+    return res.send(student);
+  } catch (e) {
+    return res.status(400).send(e.message);
+  }
+}
+
+async function declineCourse(req, res) {
+  /**
+   * student id
+   */
+  const id = req.params.id;
+  const result = validateEnrollmentReq(req.body);
+  if (result.error) {
+    return res.status(400).json({ msg: result.error.details });
+  }
+  try {
+    const { courseId } = req.body;
+    const [student, course] = await Promise.all([
+      Student.findById(id),
+      Course.findById(courseId),
+    ]);
+    if (!student) {
+      return res.status(404).send("the student with the given id not found");
+    }
+    if (!course) {
+      return res.status(404).send("the course with the given id not found");
+    }
+    student.courses.pull(course);
+    course.students.pull(student);
+    await Promise.all([student.save(), course.save()]);
+    return res.send(student);
+  } catch (e) {
+    return res.status(400).send(e.message);
+  }
+}
+
 export {
-  addStudent,
+  create,
   deleteStudent,
   editStudentById,
   getAllStudent,
   geStudentById,
+  enrollInCourse,
+  declineCourse,
 };
